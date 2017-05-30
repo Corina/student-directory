a28a11600b61c38832de06f7e291393ff84dc98c
Corina <fluturelcorina@gmail.com>
2017-05-30 18:48:00 +0100

    9. Updated student directory - added interactive menu

--- directory.rb
+++ directory.rb
@@ -105,9 +105,29 @@ def print_footer(students)
   end
 end
 
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. List the students"
+    puts "9. Exit"
+    #2. read and save the Input
+    selection = gets.chomp
+    #3. do what user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header(students)
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
 
-#nothing happens until we call the method
-students = input_students
-print_header(students)
-print(students)
-print_footer(students)
+interactive_menu
